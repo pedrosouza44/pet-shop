4d105e757851a2827339f271347f6ce376559bb0
pedrosouza44 <pedro_souza44@hotmail.com>
2026-02-19 18:07:26 -0300

    chore: remove component project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 type Props = {
   title: string;
-}
+};
 
 const Component = ({ title }: Props) => {
   console.log(title);
@@ -9,15 +9,15 @@ const Component = ({ title }: Props) => {
     <>
       <h2>Component</h2>
     </>
-  )
-}
+  );
+};
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <h2>Rocketseat</h2>
 
-      <Component title={1} />
+      <Component title={'Teste'} />
     </div>
   );
 }
